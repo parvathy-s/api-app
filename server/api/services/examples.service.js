f619340c5d1d3f2d37a80e21f2fad07e79a5f825
Parvathy Sajeev <parvathysajeev0@gmail.com>
2022-02-24 15:32:15 +0530

    test

--- server/api/services/examples.service.js
+++ server/api/services/examples.service.js
@@ -1,20 +1,10 @@
 import l from '../../common/logger';
 import db from './examples.db.service';
 
-const cors = require('cors');
-const { Pool } = require('pg');
-
-
 class ExamplesService {
   all() {
-    const db = new Pool({
-      connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false
-      }
-    });
-    const { rows } = db.query(`SELECT id, name, Description__c, extid__c FROM salesforce.example__c`);
-    return rows;
+    l.info(`${this.constructor.name}.all()`);
+    return db.all();
   }
 
   byId(id) {
